e3d298d580466661bad9c8a38fcbad10a702e959
Deaa Aldin Alawad <108593938+deaamanger@users.noreply.github.com>
2023-05-22 04:17:18 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBqByDI-MFyqab_2EhSsSzg7OJ1YY3j4Z0",
-    authDomain: "netflex-clone-a2c65.firebaseapp.com",
-    projectId: "netflex-clone-a2c65",
-    storageBucket: "netflex-clone-a2c65.appspot.com",
-    messagingSenderId: "536947691822",
-    appId: "1:536947691822:web:2de46736a45298b5f0aabf",
-    measurementId: "G-PF74LNP57P"
+   
   };
   
 
